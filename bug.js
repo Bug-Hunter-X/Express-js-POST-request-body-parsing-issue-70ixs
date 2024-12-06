7d88589e4058f36e3356f554150ed485f48a9f1c
BugHunter-X <askzenai@gmail.com>
2024-12-06 22:08:07 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+app.post('/users', (req, res) => {
+  const user = req.body;
+  // ... process the user data ...
+  res.status(201).send();
+});
+
+// ... other routes ...
+
+app.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
